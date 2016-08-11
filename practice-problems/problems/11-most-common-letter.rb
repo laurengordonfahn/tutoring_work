80f6aa8b5f4d31f422e3b04c2ac828710c499a81
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-08-10 18:20:28 -0700

    more homeworks

--- practice-problems/problems/11-most-common-letter.rb
+++ practice-problems/problems/11-most-common-letter.rb
@@ -1,3 +1,4 @@
+
 # Write a method that takes in a string. Your method should return the
 # most common letter in the array, and a count of how many times it
 # appears.
@@ -5,6 +6,34 @@
 # Difficulty: medium.
 
 def most_common_letter(string)
+	end_hash = {}
+	count = 0
+	work_array = string.split('')
+	
+
+	work_array.each do |letter|
+		count = work_array.count(letter)
+		end_hash[letter] = count
+	end
+	
+	puts end_hash
+
+	i = 0
+	most_often = end_hash[work_array[0]]
+	ans = []
+	
+	end_hash.each do |letter, count|
+		if most_often <=  end_hash[letter]
+			most_often = end_hash[letter]
+			ans = [letter, count]
+		else
+			first = work_array[0]
+			second = end_hash[first]
+			ans = [first, second]
+		end
+	end
+	puts ans
+	return ans
 end
 
 # These are tests to check that your code is working. After writing
