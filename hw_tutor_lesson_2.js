5f2c2ca6d8a8ff399d565cf3a9eb53940f4c326e
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-08-11 19:05:30 -0700

    more homeworks look at mit_hw_1.js

--- hw_tutor_lesson_2.js
+++ hw_tutor_lesson_2.js
@@ -2,31 +2,36 @@
 // area = square root of (p(p-a)(p-b)(p-c))
 // where p = (a+b+c)/2
 
-var a = 5;
-var b = 6;
-var c = 7;
-var p = (a + b + c)/2;
+// var a = 5;
+// var b = 6;
+// var c = 7;
+// var p = (a + b + c)/2;
 
-var area = Math.sqrt(p(p-a)(p-b)(p-c));
+// var area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
 
-console.log("The area of the triangle with sides" + a + b + c + "is" + area);
+// console.log("The area of the triangle with sides " + a + " "+ b + " " + c + " is " + area);
 
 
 // convert temperture from celsius to fahrenheit
 // convertion c/5 = (f-32)/9
 // test drive 60 celsius is 140 fahrenheit, 45 fahrenheit is 7.222222222222222 celsius
 
-var c = 60;
-var f = nil;
 
-if (c ===60);
+function fahrenheit(c)
 	{
-		f = (((c/5)/9) + 32);
+		f = (((c/5) *9) + 32);
 	 	console.log(f);
 	}
-elsif (f === 45);
+
+
+
+function celsius(f)
 	{
-		c = (((f-32)/9)/5);
+		c = (((f-32)/9) * 5);
 		console.log(c);
 	}
 
+
+
+fahrenheit(60);
+celsius(45);
--- mit_hw_1.js
+++ mit_hw_1.js
@@ -1,33 +1,29 @@
 
 // this should offer us the thousandth prime number starting at n = 2
-var n = 2
+var n = 3;
+var i = 1;
 
+var prime_array = new Array();
+//prime_array[0]=n;
+//console.log(prime_array);
 
-var prime_array = []
-
-until (prime_array.length === 1000)
+while (prime_array.length < 1000)
 {
 
-	if (n/2 !== 0)
+	if (n%2 !== 0)
+	{
+		for (i = 2; i < Math.sqrt(n); i ++)
 		{
-			for (i = 1, i < Math.sqrt(n); i ++);
-			{if (n%i === 0)
-				{prime_array = prime_array + n;
-			}
-			else
-				{prime_array = prime_array;
+			if (n%i === 0)
+			{
+				prime_array = prime_array.push(n);
+				
 			}
 		}
 	}
-	elsif(n === 2)
-		{prime_array = prime_array + n;
-	}	
-	else
-		{prime_array = prime_array;
-	}
 
-	n++1;
+	n++;
 }
-
-console.log("This is the 1000ths prime number" + prime_array[1000] + ".")
-console.log("this is the array of the first 1000 prime numbers" + prime_array)
+console.log(n);
+console.log(prime_array[1]);
+//console.log("this is the array of the first 1000 prime numbers" + prime_array)
