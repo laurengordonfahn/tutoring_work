b02fdaf69d83e867a8c5b7c3714ae6b551b2d29e
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-08-15 12:04:05 -0700

    added the ask for numbers from user portion

--- practice-problems/problems/10-third-greatest.rb
+++ practice-problems/problems/10-third-greatest.rb
@@ -4,6 +4,17 @@
 #
 # Difficulty: medium.
 
+def getnums
+	puts "Give me a number"
+	num1 = gets.chomp
+	puts "Give me a second number"
+	num2 = gets.chomp
+	puts "Give me a third number"
+	num3 = gets.chomp
+
+	return nums = [num1, num2, num3]
+end
+
 def biggestnumber_delete(nums)
 	i = 0
 	big1 = nums[0]
@@ -23,7 +34,8 @@ end
 	
 	
 	
-def third_greatest(nums)
+def third_greatest
+	nums = getnums
 	2.times do
 		biggestnumber_delete(nums)
 	end
@@ -40,24 +52,25 @@ def third_greatest(nums)
 	i+=1
 	end
 	
-
+	puts big1
 	return big1	 
 end
 
 
+third_greatest
 	
 # # These are tests to check that your code is working. After writing
 # # your solution, they should all print true.
 
-puts(
-  'third_greatest([5, 3, 7]) == 3: ' +
-  (third_greatest([5, 3, 7]) == 3).to_s
-)
-puts(
-  'third_greatest([5, 3, 7, 4]) == 4: ' +
-  (third_greatest([5, 3, 7, 4]) == 4).to_s
-)
-puts(
-  'third_greatest([2, 3, 7, 4]) == 3: ' +
-  (third_greatest([2, 3, 7, 4]) == 3).to_s
-)
+# puts(
+#   'third_greatest([5, 3, 7]) == 3: ' +
+#   (third_greatest([5, 3, 7]) == 3).to_s
+# )
+# puts(
+#   'third_greatest([5, 3, 7, 4]) == 4: ' +
+#   (third_greatest([5, 3, 7, 4]) == 4).to_s
+# )
+# puts(
+#   'third_greatest([2, 3, 7, 4]) == 3: ' +
+#   (third_greatest([2, 3, 7, 4]) == 3).to_s
+# )
