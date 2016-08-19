9e4d08f7d3139184680e3fffe4ddbe7f4d1e1f34
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-08-19 11:28:03 -0700

    Got bubbleing to work in ruby

--- bubbling.rb
+++ bubbling.rb
@@ -19,29 +19,29 @@ end
 
 # puts switch(["a", "b", "c", "d","e", "f"], [3, 4])
 
+
+
 def bubbleing(array)
-	
 	no_switch = 0
-	while switch_count != ((array.length) -1)
+	while no_switch < ((array.length) -1)
 		i = 0
 		j = 1
 		while j < array.length
-			
 			if array[i] > array[j]
-				array = switch(array,[i,j])
-			
+				array = switch(array, [i, j])
+
 			else
 				no_switch = no_switch + 1
-				
 			end
-			j +=1
 			i +=1
+			j +=1
+		end
+		if no_switch != ((array.length) -1)
+			no_switch = 0 
 		end
 	end
-
 	return array
 end
 
-puts bubbleing([2, 11, 10])
-
+puts bubbleing([2, 11, 10, 4, 1])
 
